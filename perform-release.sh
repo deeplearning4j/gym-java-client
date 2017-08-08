718fcc95a073f07618838ec79c1ab406a2614d4c
Samuel Audet <samuel.audet@gmail.com>
2017-08-08 09:50:21 +0900

    Call git commit and tag with -s in perform-release.sh

--- perform-release.sh
+++ perform-release.sh
@@ -27,13 +27,13 @@ if [[ "${SKIP_BUILD}" == "0" ]]; then
 mvn clean deploy -Dgpg.executable=gpg2 -DperformRelease -Psonatype-oss-release -DskipTests -DstagingRepositoryId=$STAGING_REPOSITORY
 fi
 
-git commit -a -m "Update to version $RELEASE_VERSION"
-git tag -a -m "gym-java-client-$RELEASE_VERSION" "gym-java-client-$RELEASE_VERSION"
-git tag -a -f -m "gym-java-client-$RELEASE_VERSION" "latest_release"
+git commit -s -a -m "Update to version $RELEASE_VERSION"
+git tag -s -a -m "gym-java-client-$RELEASE_VERSION" "gym-java-client-$RELEASE_VERSION"
+git tag -s -a -f -m "gym-java-client-$RELEASE_VERSION" "latest_release"
 
 sed -i "s/<nd4j.version>.*<\/nd4j.version>/<nd4j.version>$SNAPSHOT_VERSION<\/nd4j.version>/" pom.xml
 sed -i "s/<datavec.version>.*<\/datavec.version>/<datavec.version>$SNAPSHOT_VERSION<\/datavec.version>/" pom.xml
 mvn versions:set -DallowSnapshots=true -DgenerateBackupPoms=false -DnewVersion=$SNAPSHOT_VERSION
-git commit -a -m "Update to version $SNAPSHOT_VERSION"
+git commit -s -a -m "Update to version $SNAPSHOT_VERSION"
 
 echo "Successfully performed release of version $RELEASE_VERSION ($SNAPSHOT_VERSION) to repository $STAGING_REPOSITORY"
